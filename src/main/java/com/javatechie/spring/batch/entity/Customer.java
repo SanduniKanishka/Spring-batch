c691ae406471993de263555409b196766d839015
SanduniKanishka <skchamodani@gmail.com>
2024-05-20 00:55:05 +0200

    Spring Batch project

---
+++ src/main/java/com/javatechie/spring/batch/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.javatechie.spring.batch.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import javax.lang.model.element.Name;
+
+@Entity
+@Data
+@Table(name = "CUSTOMER_INFO")
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "GENDER")
+    private String gender;
+
+    @Column(name = "CONTACT_NO")
+    private String contactNo;
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @Column(name = "DOB")
+    private String dob;
+
+}
